5c298b1ef091dd8090e41972ff9b39bf9a6f8220
ludyellen <160621081+ludyellen@users.noreply.github.com>
2024-11-21 10:37:03 -0300

    21

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'cosméticos',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    ' Quais ingredientes em cosméticos ajudam a melhorar a firmeza e a elasticidade da pele? ',
+    ': Ingredientes como retinol, vitamina C, ácido hialurônico, peptídeos e colágeno são eficazes para melhorar a firmeza e elasticidade da pele. O retinol estimula a produção de colágeno e acelera a renovação celular, enquanto a vitamina C é um potente antioxidante que protege e melhora a firmeza. O ácido hialurônico hidrata e preenche a pele, enquanto os peptídeos e o colágeno ajudam a reforçar a estrutura da pele, tornando-a mais firme.'
 )
 
 criaCartao(
     'cosméticos',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    '2. Como escolher o melhor protetor solar para peles maduras e com tendência a manchas?',
+    'Para peles maduras e com tendência a manchas, escolha um protetor solar com fator de proteção solar (FPS) de no mínimo 30, preferencialmente FPS 50 ou mais. Além disso, busque opções com proteção de amplo espectro (contra raios UVA e UVB)'
 )
 
 criaCartao(
     'cosméticos',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Onde os cosméticos atuam?',
+    'Os produtos cosméticos são desenvolvidos para auxiliar no tratamento de pele, cabelos, unhas e demais partes do corpo, como por exemplo, mãos e pés.'
 )
 
 criaCartao(
     'cosméticos',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual a origem dos cosméticos?',
+    'Muitos cosméticos se originaram na Ásia, mas os primeiros registros de seu uso estão no Egito. Mais ou menos no ano de 180 d.C., na era Romana, um médico grego chamado Claudius Galen realizou sua própria pesquisa na mani- pulação de produtos cosméticos, iniciando, assim, a era dos produtos químico-farmacêuticos'
 )
