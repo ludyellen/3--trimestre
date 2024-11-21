fb8211a4b2a95e0efa055b183a850fa7881aabeb
ludyellen <160621081+ludyellen@users.noreply.github.com>
2024-11-21 09:59:21 -0300

    8

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
+    'cosméticos',
     'O que é Python?',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'Geografia',
+    'cosméticos',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
+    'cosméticos',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'cosméticos',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
